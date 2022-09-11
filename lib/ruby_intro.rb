3e16be1471fd992eb12951927a5905114c71a583
kkeen699 <kkeen699@gmail.com>
2022-09-10 23:11:59 -0500

    update

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -3,34 +3,70 @@
 # Part 1
 
 def sum(arr)
-  # YOUR CODE HERE
+  sum = 0
+  arr.each{
+    |i|
+    sum += i
+  }
+  sum
 end
 
 def max_2_sum(arr)
-  # YOUR CODE HERE
+  if arr.length() == 0
+    0
+  elsif arr.length == 1
+    arr[0]
+  else
+    sum(arr.max(2))
+  end
 end
 
 def sum_to_n?(arr, number)
-  # YOUR CODE HERE
+  dic = {}
+  arr.each{
+    |i|
+    if dic[i]
+      return true
+    else
+      dic[number-i] = i
+    end
+  }
+  return false
 end
 
 # Part 2
 
 def hello(name)
-  # YOUR CODE HERE
+  "Hello, #{name}"
 end
 
 def starts_with_consonant?(string)
-  # YOUR CODE HERE
+  string[0] =~ /[^aeiou\W]/i
 end
 
 def binary_multiple_of_4?(string)
-  # YOUR CODE HERE
+  if not string =~ /[^10+-]/ and not string.empty?
+    string.to_i(2) % 4 == 0
+  end
 end
 
 # Part 3
 
 # Object representing a book
 class BookInStock
-  # YOUR CODE HERE
+  def initialize(isbn, price)
+    if isbn.empty? or price <= 0
+      raise ArgumentError
+    end
+
+    @isbn = isbn
+    @price = price
+  end
+
+  attr_accessor :isbn
+  attr_accessor :price
+
+  def price_as_string
+    "$#{'%.2f' % @price}"
+  end
 end
